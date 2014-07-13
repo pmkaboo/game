07f3ffcf1f4786538d3c42cb22d804a546ae1161
prales <pralovski@gmail.com>
2014-07-13 22:46:36 +0200

    Extented tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,25 @@
 require "rails_helper" 
 
-describe HomeController  do
-  
-  context "GET index" do 
+describe HomeController do
+
+  describe "GET index" do
     it "returns http success" do
       get :index
-      response.should be_success
-    end
-  end  
-  
+      expect(response).to render_template("index")      
+    end   
+
+    it "evaluates variables" do
+      get :index
+      expect(assigns[:game_data]).to be_a Hash
+      expect(assigns[:money]).to be_a Integer	
+    end 	
+  end
+
+  context "POSTS delete_cookies" do
+  	it "redirects after deleting cookies" do
+  	  post :delete_cookies
+  	  expect(response).to redirect_to("/")	
+  	end	
+  end	
+
 end
